ae079437fd2e1a8d8621f791197c6dda85362e49
JR Cologne <habichvergessen40@gmail.com>
2017-05-07 19:02:16 +0200

    Add newer version of db-class

--- php/db.php
+++ php/db.php
@@ -2,7 +2,5 @@
 	require_once('config.php');
   require_once('includes/db-class/DB.php');
 
-  DB::initErrorHandler([], 'development');
-
 	$db = new DB(DB_NAME, DB_USER, DB_PASSWORD, DB_TYPE, DB_HOST, PDO_ERROR_MODE);
 ?>
