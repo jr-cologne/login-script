3cbb917de493dd09c4f3a1e623fa95c2afcc45cb
JR Cologne <kontakt@jr-cologne.de>
2017-11-10 23:07:26 +0100

    Update Dependencies

--- includes/db.php
+++ includes/db.php
@@ -1,3 +1,8 @@
 <?php
-	$db = new JRCologne\Utils\DB(DB_NAME, DB_USER, DB_PASSWORD, DB_TYPE, DB_HOST, PDO_ERROR_MODE);
+  use JRCologne\Utils\Database\DB;
+  use JRCologne\Utils\Database\QueryBuilder;
+
+	$db = new DB(new QueryBuilder);
+
+  $db->connect(DB_TYPE . ':dbname=' . DB_NAME . ';host=' . DB_HOST . ';charset=utf8', DB_USER, DB_PASSWORD);
 ?>
