3cc231342248220aabcbe679a1a137052b10c07f
JR Cologne <kontakt@jr-cologne.de>
2018-03-16 18:18:11 +0100

    Clear up

--- includes/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-  use JRCologne\Utils\Database\DB;
-  use JRCologne\Utils\Database\QueryBuilder;
-
-	$db = new DB(new QueryBuilder);
-
-  $db->connect(DB_TYPE . ':dbname=' . DB_NAME . ';host=' . DB_HOST . ';charset=utf8', DB_USER, DB_PASSWORD);
-?>
--- includes/session.php
+++
@@ -1,6 +0,0 @@
-<?php
-  // start session
-  session_start();
-  // regenerate session id
-  session_regenerate_id();
-?>
